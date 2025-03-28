def8b009fc793a779a1a74877281205f7549d794
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-03-28 14:30:14 -0500

    Add includeFollowupRequests parameter to request candidates payload.

--- src/scanning/scanning.requests.js
+++ src/scanning/scanning.requests.js
@@ -55,6 +55,7 @@ export async function searchCandidates({
       startDate,
       endDate: endDate || "",
       queryID: queryID || "",
+      includeFollowupRequests: "true",
     },
   });
   return {
