423e67c882976dd98d2b35d8f7f9d2e7090d4514
antoine-le-calloch <antoine.lecalloch@gmail.com>
2025-04-02 23:42:30 -0500

    Add allocation selection to RequestFollowup component and add a style to allow wrap when select option is too long.

--- src/scanning/scanningSession/components/RequestFollowup/RequestFollowup.jsx
+++ src/scanning/scanningSession/components/RequestFollowup/RequestFollowup.jsx
@@ -1,4 +1,6 @@
 import "./RequestFollowup.scss";
+import { useAllocationsApiClassname, useUserAccessibleGroups } from "../../../../common/common.hooks.js";
+import { IonItem, IonList, IonSelect, IonSelectOption } from "@ionic/react";
 
 /**
  * Component for requesting follow-up from a candidate.
@@ -8,7 +10,35 @@ import "./RequestFollowup.scss";
  * @returns {JSX.Element}
  */
 export const RequestFollowup = ({candidate, modal}) => {
+  const { allocationsApiClassname } = useAllocationsApiClassname()
+  const { userAccessibleGroups } = useUserAccessibleGroups();
+
   return (
-    <div></div>
+    <IonList className="request-followup">
+      <IonItem>
+        <IonSelect
+          label="Allocation" labelPlacement="stacked"
+          placeholder="Select allocation"
+          interface="modal"
+          interfaceOptions={{
+            initialBreakpoint: 0.75,
+            breakpoints: [0, 0.25, 0.5, 0.75, 1],
+            expandToScroll: false,
+          }}
+         className="allocation-select"
+          style={{
+
+          }}
+
+        >
+          {allocationsApiClassname?.map((allocation) => (
+            <IonSelectOption value={allocation} key={allocation.id} className="allocation-option">
+              {allocation.instrument?.telescope?.name + "\n"}
+              {allocation.instrument?.name} - {userAccessibleGroups?.find((group) => group.id === allocation.group_id)?.name} (PI {allocation.pi})
+            </IonSelectOption>
+          ))}
+        </IonSelect>
+      </IonItem>
+    </IonList>
   );
 };
